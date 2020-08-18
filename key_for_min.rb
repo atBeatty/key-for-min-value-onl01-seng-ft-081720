f8aac00d8fba06c39d3e84c4eee3d49c3ce422c4
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-18 19:32:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+
 values = []
+
   name_hash.each do |key, value|
     values << value
+    if values.length == 0
+      return nil
+    end
+    
   end
   smallest_value = values[0]
   values.each do |val|
