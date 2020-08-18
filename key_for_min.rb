1f789074a68332e2ce666dd38bf97837030da11b
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-18 19:36:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,18 +8,21 @@ values = []
   name_hash.each do |key, value|
     values << value
   end
-  smallest_value = values[0]
-  values.each do |val|
-    if val < smallest_value
-      smallest_value = val
+
+  if values.size == 0
+    nil
+  else
+    smallest_value = values[0]
+    values.each do |val|
+      if val < smallest_value
+        smallest_value = val
+      end
+    end
+
+    name_hash.each do |key, value|
+      if value == smallest_value
+        return key
+      end
     end
   end
-
-  name_hash.each do |key, value|
-    if value == smallest_value
-      return key
-    end
-  end
-
-
 end
